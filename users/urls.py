ee741e377a6ff70bfed43cf6bda86625521945e5
ayaemx <aya3mad2002@gmail.com>
2025-05-29 15:14:59 +0300

    Add UserSerializer and user list API endpoint and requirements file

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import UserRegisterView, UserProfileEditView , ActivateAccountView , email_login_view , UserDeleteView , UserProfileView
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView
+from .api_views import UserListAPI
 
 app_name = 'users'
 urlpatterns = [
@@ -12,5 +13,6 @@ urlpatterns = [
     path('login/', email_login_view, name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('delete/', UserDeleteView.as_view(), name='delete-account'),
+    path('api/users/', UserListAPI.as_view(), name='api-user-list'),
 
 ]
