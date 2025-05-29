1d13db264cd8b7c358fcad103cc58972639f2049
Menna Khalaf <mennatullahmdhg@gmail.com>
2025-05-29 22:36:03 +0300

    amended project view and templates and serializer

--- crowdfunding_projectt/urls.py
+++ crowdfunding_projectt/urls.py
@@ -21,9 +21,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+ path('api/', include('projects.api_urls')),
+      
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('users/', include('users.urls', namespace='users')),
+
+
+     path('projects/', include('projects.urls', namespace='projects')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
